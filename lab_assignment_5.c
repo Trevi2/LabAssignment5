44610f6e966c3e3ad9e245f49fe09c3fe6d5eb7b
Trevi Meyer <tr063063@ucf.edu>
2024-02-15 12:59:46 -0500

    Initial commit

---
+++ lab_assignment_5.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	char letter;
+	struct node* next;
+} node;
+
+// Returns number of nodes in the linkedList.
+int length(node* head)
+{
+	int count = 0;
+
+	while(head != NULL)
+	{
+		count++;
+		head = head->next;
+	}
+
+	return count;
+}
+
+// parses the string in the linkedList
+//  if the linked list is head -> |a|->|b|->|c|
+//  then toCString function will return "abc"
+char* toCString(node* head)
+{
+	int size = length(head);
+
+	node *temp = head;
+
+	char *arr = (char*)malloc((size + 1) * sizeof(char));
+	int x = 0;
+
+	while(temp != NULL)
+	{
+		arr[x++] = temp->letter;
+			
+		temp = temp->next;		
+	}
+	arr[x] = '\0';
+
+	return arr;
+}
+
+// inserts character to the linkedlist
+// if the linked list is head -> |a|->|b|->|c|
+// then insertChar(&head, 'x') will update the linked list as follows:
+// head -> |a|->|b|->|c|->|x|
+void insertChar(node** pHead, char c)
+{
+	node *newNode = (node*)malloc(sizeof(node));
+	newNode->letter = c;
+	newNode->next = NULL;
+	
+	if(*pHead == NULL)
+	{
+		*pHead = newNode;
+	}
+	else
+	{
+		node *temp = *pHead;
+
+		while(temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+			
+		temp->next = newNode;
+	}
+
+}
+
+// deletes all nodes in the linkedList.
+void deleteList(node** pHead)
+{
+	while(*pHead != NULL)
+	{
+		node * current = *pHead;
+		*pHead = (*pHead)->next;
+
+		free(current);
+	}
+}
+
+int main(void)
+{
+	int i, strLen, numInputs;
+	node* head = NULL;
+	char* str;
+	char c;
+	FILE* inFile = fopen("input.txt","r");
+
+	fscanf(inFile, " %d\n", &numInputs);
+	
+	while (numInputs-- > 0)
+	{
+		fscanf(inFile, " %d\n", &strLen);
+		for (i = 0; i < strLen; i++)
+		{
+			fscanf(inFile," %c", &c);
+			insertChar(&head, c);
+		}
+
+		str = toCString(head);
+		printf("String is : %s\n", str);
+
+		free(str);
+		
+		deleteList(&head);
+
+		if (head != NULL)
+		{
+			printf("deleteList is not correct!");
+			break;
+		}
+	}
+
+	fclose(inFile);
+}
